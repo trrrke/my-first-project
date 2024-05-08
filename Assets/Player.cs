4c3050cc14f75c44b3c61a4978e79e2dc7bd674c
trrrke <qzp8@hotmail.com>
2024-05-08 13:46:25 +0300

    jumpforce

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -1,42 +1,26 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.PlayerLoop;
 
 public class Movement : MonoBehaviour
 {
-    public Rigidbody Rigidbody;
-  //  public CharacterController controller;
-    public class PlayerController : MonoBehaviour
-    {
-        public float JumpForce = 10f;
-      // private Rigidbody Rigidbody;
-    }
+    public Rigidbody rigidbody;
+    public float jumpForce = 10f;
+
     // Start is called before the first frame update
     void Start()
     {
-        Rigidbody = GetComponent<Rigidbody>();
-       // Rigidbody = gameObject.GetComponent<Rigidbody>();
-      //  controller = gameObject.GetComponent<CharacterController>();
+        rigidbody = GetComponent<Rigidbody>();
     }
-
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButtonDown("jump"))
+        if (Input.GetButton("jumpForce"))
         {
-            Jump();
+            rigidbody.AddForce(Vector3.up * jumpForce);
+            Debug.Log("hello");
         }
-       // float Horizontal = Input.GetAxis("Horizontal");
-       // float Vertical = Input.GetAxis("Vertical");
-       // Vector3 Movement = new Vector3(Horizontal, 1, Vertical);
-       // Rigidbody.MovePosition(Rigidbody.position + Movement*5* Time.deltaTime);
     }
-    void Jump()
-    {
-        Rigidbody.velocity = new Vector3(Rigidbody.velocity.x, jumpForce , Rigidbody.velocity.z);
-    }
-
 }
+
