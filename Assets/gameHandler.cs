956f0acfaa4a59ba796a0613c09e50cb98181b0f
trrrke <qzp8@hotmail.com>
2024-05-13 11:51:11 +0300

    heal and damage

---
+++ Assets/gameHandler.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using UnityEngine.UI;
+
+public class gameHandler : MonoBehaviour
+{
+    public Slider slider;
+    HelathSystem healthSystem = new HelathSystem(100);
+    // Start is called before the first frame update
+    public void Update()
+    {
+        slider.value = healthSystem.gethealthpercentage();
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag =="enemy") { healthSystem.Damage(10); }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "heal") {  healthSystem.heal(10); }
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+
+}
---
+++ Assets/health system.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using UnityEngine.UI;
+
+[Serializable]
+public class HelathSystem : MonoBehaviour
+{
+    private float helathPoints;
+    private float maxhelath;
+    // Start is called before the first frame update
+    public HelathSystem(int maxhelath)
+    {
+        this.maxhelath = maxhelath;
+        helathPoints = maxhelath;
+    }
+    public float gethealth() { return helathPoints; }
+    public float gethealthpercentage() { return helathPoints / maxhelath; }
+    public void Damage(int DamageAmont)
+    {
+        helathPoints -= DamageAmont;
+        if (helathPoints < 0) {helathPoints = 0;}
+    }
+    public void heal(int healAmont)
+    {
+        helathPoints += healAmont;
+        if (helathPoints > maxhelath) { helathPoints = maxhelath;}
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
